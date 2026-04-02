3e3bec98228d6ab12a6c10bad92685fc5e3d1650
jayaramakrishna986 <jayaramakrishna986@gmail.com>
2026-04-02 07:54:54 -0700

    Half Day is Done

---
+++ Week-1/Day-3/Pandas.py
@@ -0,0 +1,14 @@
+import pandas as pd
+a=pd.DataFrame([10,58,65,5,8])
+
+print(a)
+
+A={
+    "Name":["Jairam","ABC","XYZ"],
+    "Age":[25,23,21],
+    "Address":["Aditi","CDS","ADS"]
+}
+
+df=pd.DataFrame(A)
+print(df)
+
