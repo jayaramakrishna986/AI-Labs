c2c551e33b4969dfc042fcbb083e21f36491b32a
jayaramakrishna986 <jayaramakrishna986@gmail.com>
2026-05-29 23:51:29 +0530

    Workflows

--- Week-5/Day-1/Langchain_models/2.ChatModels/chatmodelhg_local.py
+++ Week-5/Day-1/Langchain_models/2.ChatModels/chatmodelhg_local.py
@@ -1,17 +1,31 @@
-from langchain_huggingface import HuggingFacePipeline,ChatHuggingFace
+# from langchain_huggingface import HuggingFacePipeline,ChatHuggingFace
 
-llm=HuggingFacePipeline.from_model_id(
-    model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
-    task='text-generation',
-    pipeline_kwargs=dict(
-        temperature=0.5,
-        max_new_tokens=100
-        )
+# llm=HuggingFacePipeline.from_model_id(
+#     model_id='TinyLlama/TinyLlama-1.1B-Chat-v1.0',
+#     task='text-generation',
+#     pipeline_kwargs=dict(
+#         temperature=0,
+#         max_new_tokens=100
+#         )
+# )
+
+# model=ChatHuggingFace(llm=llm)
+
+
+# result=model.invoke("Give me any 5 places of the india?")
+
+# print(result)
+
+
+from langchain_ollama import OllamaLLM
+
+llm = OllamaLLM(
+    model="llama3.2",
+    temperature=0
 )
 
-model=ChatHuggingFace(llm=llm)
+response = llm.invoke("Give me any 5 famous places in India.")
+
+print(response)
 
 
-result=model.invoke("What are the stated that are having high number of rivers?")
-
-print(result)
